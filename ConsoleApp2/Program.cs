c39a2d86cfbdbecc221fc9b621cd6addea8c7853
buev.ty2128 <buev.ty2128@C0M31-R1>
2024-04-04 11:29:27 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace kovariantnost
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MessageBuilder messageBuilder = WriteEM;
+
+            Message message = messageBuilder("Hello");
+            message.Print();
+
+            //ковариантность позволяет передать делегату метод возвращаемый тип которого является производным от возвращаемого типа делегата
+        }
+
+        public static EmailMessage WriteEM(string text) => new EmailMessage(text);
+
+        delegate Message MessageBuilder(string text);
+    }
+
+    public class Message
+    {
+        public string text { get; set; }
+        public Message(string _text) =>
+            text = _text;
+
+        public virtual void Print() =>
+            Console.WriteLine($"Message: {text}");
+    }
+
+    public class EmailMessage : Message
+    {
+        public EmailMessage(string _text) : base(_text) { }
+
+        public override void Print() =>
+            Console.WriteLine($"Email: {text}");
+    }
+
+    public class SMSMessage : Message
+    {
+        public SMSMessage(string _text) : base(_text) { }
+
+        public override void Print() =>
+            Console.WriteLine($"SMS Message: {text}");
+    }
+}
---
+++ kontkovatiantist/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace kontkovatiantist
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Email emailBox = SendMessage;
+            emailBox(new EmailMessage("Welcome"));
+        }
+
+        static void SendMessage(Message message) =>
+                message.Print();
+
+        delegate void Email(EmailMessage email);
+    }
+
+    public class Message
+    {
+        public string text { get; set; }
+        public Message(string _text) =>
+            text = _text;
+
+        public virtual void Print() =>
+            Console.WriteLine($"Message: {text}");
+    }
+
+    public class EmailMessage : Message
+    {
+        public EmailMessage(string _text) : base(_text) { }
+
+        public override void Print() =>
+            Console.WriteLine($"Email: {text}");
+    }
+
+    public class SMSMessage : Message
+    {
+        public SMSMessage(string _text) : base(_text) { }
+
+        public override void Print() =>
+            Console.WriteLine($"SMS Message: {text}");
+    }
+}
